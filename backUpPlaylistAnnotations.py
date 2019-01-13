d5f9d8322f904dbe26806484abb9214e72470b84
BackUpYouTube <46559713+BackUpYouTube@users.noreply.github.com>
2019-01-13 12:44:19 +0300

    Added an option to specify a hard limit
    Can now specify an upper bound to how many videos can be scanned from outside the playlist

--- backUpPlaylistAnnotations.py
+++ backUpPlaylistAnnotations.py
@@ -38,6 +38,9 @@ i=0
 toGather=0
 t = threading.Timer(10.0, reportProgress, [i,toGather])
 
+hardLimitSet=False
+hardLimit = 0
+
 def makeRequest(partialURL,parameter,ID):
 	pars = {"parameter" : ID}
 	return requests.get(partialURL, params=pars)
@@ -125,6 +128,13 @@ def main():
 			
 			p = p[start:end]
 
+			#Adjust hard limit so it goes beyond the playlist
+			hardLimitSet = False 
+			err = setHardLimit()
+			t.cancel()
+			if err ==1:
+				continue
+
 			#vID = idExtractor(argument)
 			#print("vID: {}".format(vID))
 			#r = annotationsBackedUp(vID)
@@ -204,7 +214,7 @@ def gatherStartingFromPlaylistVids():
 		q.append(video)
 		m[video] = False
 
-	while len(q) != 0:
+	while len(q) != 0 and not (hardLimitSet and len(m) >= hardLimit):
 		head = q.pop()
 		code = gather(head)
 		#check for errors
@@ -223,7 +233,8 @@ def gatherStartingFromPlaylistVids():
 			if action == 'a':
 				return 1
 		#q.pop()
-
+	if (hardLimitSet and len(m) >= hardLimit):
+		print("HARDLIMIT of {} reached! Scan halted!".format(len(m)))
 	#print (m)
 	return 0
 def analyzePlaylist(pID):
@@ -334,6 +345,8 @@ def gather(vID):
 			if not lId in m:
 				m[lId] = False
 				q.append(lId)
+				if(hardLimitSet and len(m) >= hardLimit):
+					return 0
 				#print("added {} to queue".format(lId))
 			#else:
 				#print("duplicate entry detected")
@@ -368,6 +381,41 @@ def backUp(vID):
 		return 1
 	else:
 		return 0
+
+def setHardLimit():
+	global hardLimit,hardLimitSet
+	print("Throughly scanning links can sometimes generate thousands of videos.")
+	print("To spare your computer we offer the option of setting a hard limit to how many videos its allowed to scan.")
+	print("(Besides videos already included in the playlist)")
+	print("Although this is largely dependent on what video you're scanning and on your internet speed")
+	print("We found that the average speed was about 10000 videos/hour on our devices.")
+	print("Keep in mind that this is only the discovery process and that nothing is backed up in this phase.")
+	action=""
+	while True:
+		print("Please type a single number specifying the maximum number of videos to be scanned")
+		print("Or hit enter to scan everything available. (Do this only if you know what you're doing)")
+		action = input("")
+		action = action.rstrip().strip()
+		if action == '':
+			return 0
+		elif isASingleNumber(action):
+			hardLimitSet = True
+			hardLimit = getTheNumber(action)+len(p)
+			if hardLimit==1:
+				print("Limitted scan to 1 video.")
+			else:
+				print("Limitted scan to {} videos.".format(str(hardLimit)))
+			return 0
+
+
+
+def isASingleNumber(inp):
+	return ( len(re.findall('(?!\-|\.)[0-9]+',inp)) == 1) and ( int(re.findall('(?!\-|\.)[0-9]+',inp)[0]) > 0)
+
+def getTheNumber(inp):
+	numberList = re.findall('(?!\-|\.)[0-9]+',inp)
+	return int(numberList[0])
+
 
 #Interval functions
 def contains2Numbers(inp):
